85e70c1692aa27d4b5539687bcec0dab08a328de
veinthrough <veinthrough@gmail.com>
2017-04-17 17:01:21 +0800

    Add GetOpt and GetOptDesc.

---
+++ src/com/veinthrough/lang/GetOptDesc.java
@@ -0,0 +1,38 @@
+package com.veinthrough.lang;
+
+/** A GetOptDesc describes one argument that may be accepted by the program.
+ */
+public class GetOptDesc {
+    /** The short-form option letter */
+    private char argLetter;
+    /** The long-form option name */
+    private String argName;
+    /** True if this option needs an argument after it */
+    private boolean takesArgument;
+
+    /** Construct a GetOpt option.
+     * @param ch The single-character name for this option.
+     * @param nm The word name for this option.
+     * @param ta True if this option requires an argument after it.
+     */
+    public GetOptDesc(char ch, String nm, boolean ta) {
+        if (!Character.isLetter(ch) && !Character.isDigit(ch)) {
+            throw new IllegalArgumentException(ch + ": not letter or digit");
+        }
+        argLetter = ch;
+        argName   = nm; // may be null, meaning no long name.
+        takesArgument = ta;
+    }
+
+    public char getArgLetter() {
+        return argLetter;
+    }
+
+    public String getArgName() {
+        return argName;
+    }
+
+    public boolean takesArgument() {
+        return takesArgument;
+    }
+}
---
+++ src/com/veinthrough/util/Debug.java
@@ -0,0 +1,51 @@
+package com.veinthrough.util;
+
+/** Utilities for debugging
+ * @author  Ian Darwin, http://www.darwinsys.com/
+ */
+@Deprecated
+public class Debug {
+    private static final String DEBUG_STRING = "debug.";
+
+    /** Static method to see if a given category of debugging is enabled.
+     * Enable by setting e.g., -Ddebug.fileio to debug file I/O operations.
+     * For example:<br>
+     * if (Debug.isEnabled("fileio"))<br>
+     *  System.out.println("Starting to read file " + fileName);
+     * @param category The category
+     * @return True if the category is enabled for debuggin
+     */
+    public static boolean isEnabled(String category) {
+        return System.getProperty(DEBUG_STRING + category) != null;
+    }
+
+    /** Static method to println a given message if the
+     * given category is enabled for debugging, as reported by isEnabled.
+     * @param category The category
+     * @param msg The message
+     */
+    public static void println(String category, String msg) {
+        if (isEnabled(category))
+            System.out.println(msg);
+    }
+
+    /** Static method to println an arbitrary Object if the given
+     * category is enabled for debugging, as reported by isEnabled.
+     * @param category The category
+     * @param stuff The message
+     */
+    public static void println(String category, Object stuff) {
+        println(category, stuff.toString());
+    }
+
+    /** Invoke System.out.printf if and only if the given
+     * category is enabled for debugging, as reported by isEnabled.
+     * @param category The category
+     * @param format The format string
+     * @param objects The parameters for the format string
+     */
+    public static void printf(String category, String format, Object...objects) {
+        if (isEnabled(category))
+            System.out.printf(format, objects);
+    }
+}
